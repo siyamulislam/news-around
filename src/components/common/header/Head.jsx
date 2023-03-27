de3bceaf3f903db3ead8420f842c7a918e42b5ac
siyamulislam <siyamulislam1@gmail.com>
2023-03-27 23:45:39 +0600

    template init

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB paddingTB '>
+          <div className='logo'>
+            <img src='../images/logo2.png' alt='' style={{height:'100px',width:'250px'}} />
+          </div>
+          <div className='ad'>
+            <img src='../images/bnad.png' alt=''  style={{height:'100px',width:'545px'}}/>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
